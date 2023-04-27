192e13fcc2b3b2e7bd6c0105f0a75b18a76a1236
NurazizahI <130164909+NurazizahI@users.noreply.github.com>
2023-04-27 17:55:26 +0700

    Update web.py

--- web.py
+++ web.py
@@ -23,3 +23,16 @@ if (selected == 'Hitung Nilai Normalitas') :
         nilai_normalitas = Massa/(BE*Volume)
         st.success(f'Nilai Normalitas adalah {nilai_normalitas}')
         
+# halaman hitung nilai molaritas 
+if (selected == 'Hitung Nilai Molaritas') :
+    st.title('Hitung Nilai Molaritas')
+    
+    Massa = st.number_input('Masukan Nilai Massa')
+    Volume = st.number_input('Masukan Nilai Volume')
+    Mr = st.number_input('Masukan Nilai Mr')
+    
+    tombol = st.button('Hitung Nilai Molaritas')
+    
+    if tombol:
+        nilai_normalitas = Massa/(Mr*Volume)
+        st.success(f'Nilai Normalitas adalah {nilai_Molaritas}')
